773be67b9651012b0a367c002f7d2ced80d0f2f0
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-04-08 10:31:09 +0530

    update

--- js/script.js
+++ js/script.js
@@ -4,10 +4,12 @@ var greet;
 
 if(hours < 12){
 	greet = "<center><img src='images/image1.png' width='500'/></center>";
-}else if(hours >= 12 && hours <= 17){
+}else if(hours >= 12 && hours <= 16){
 	greet = "<center><img src='images/image2.png' width='500'/></center>";
-}else if(hours >= 17 && hours <= 24){
+}else if(hours >= 16 && hours <= 20){
 	greet = "<center><img src='images/image3.png' width='500'/></center>";
+}else if(hours >= 20 && hours <= 24){
+	greet = "<center><img src='images/image4.png' width='500'/></center>";
 }
 
 document.getElementById('greeting').innerHTML = greet;
