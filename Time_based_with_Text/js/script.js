7da386857d2e142487755b2f163da761271f4218
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-04-08 11:11:40 +0530

    update

--- Time_based_with_Text/js/script.js
+++ Time_based_with_Text/js/script.js
@@ -3,13 +3,13 @@ var hours = date.getHours();
 var greet;
 
 if(hours < 12){
-	greet = "<center><img src='images/image1.png' width='500'/> </br> <div> Good Morning.</div></center>";
+	greet = "<center><div> Good Morning.</div></center>";
 }else if(hours >= 12 && hours <= 16){
-	greet = "<center><img src='images/image2.png' width='500'/> </br> <div> Good Afternoon.</div></center>";
+	greet = "<center><div> Good Afternoon.</div></center>";
 }else if(hours >= 16 && hours <= 20){
-	greet = "<center><img src='images/image3.png' width='500'/> </br> <div> Good Evening.</div></center>";
+	greet = "<center><div> Good Evening.</div></center>";
 }else if(hours >= 20 && hours <= 24){
-	greet = "<center><img src='images/image4.png' width='500'/> </br> <div> Good Night.</div></center>";
+	greet = "<center><div> Good Night.</div></center>";
 }
 
 document.getElementById('greeting').innerHTML = greet;
